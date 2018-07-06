df188b09035292ae09de05d0ff70803ca4de638c
scrappy1987 <jagordon87@msn.com>
2018-07-06 10:09:05 +0100

    added bean validation

--- src/main/java/com/qa/domain/Account.java
+++ src/main/java/com/qa/domain/Account.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Account {
@@ -13,6 +14,7 @@ public class Account {
 	private Long id;
 	private String firstName;
 	private String secondName;
+	@Size(min = 6, max = 6)
 	private String accountNumber;
 
 	public Account() {
